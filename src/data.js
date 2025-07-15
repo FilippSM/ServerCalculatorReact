bb5feb339e86f7eee9b46ece42ee5b88f3a9c7fd
Filipp Sam <philippsm666@gmail.com>
2025-07-15 21:48:07 +0300

    first commit

---
+++ src/data.js
@@ -0,0 +1,12 @@
+export const tasks = [
+    { id: 1, title: "Learn Express" },
+    { id: 2, title: "Build API" },
+    { id: 3, title: "Deploy Application" },
+    { id: 4, title: "Write Tests" },
+    { id: 5, title: "Document Code" },
+    { id: 6, title: "Refactor Code" },
+    { id: 7, title: "Optimize Performance" },
+    { id: 8, title: "Add Middleware" },
+    { id: 9, title: "Handle Errors" },
+    { id: 10, title: "Implement Authentication" }
+];
---
+++ src/index.js
@@ -0,0 +1,73 @@
+import express from 'express'
+import { tasks } from "./data.js"
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Deploy on server' })
+})
+
+app.get('/tasks', (req, res) => {
+    res.json({ data: tasks })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    const taskId = parseInt(req.params.id, 10)
+
+    const task = tasks.find(task => task.id === taskId)
+
+    if (task) {
+        res.json({ data: task })
+    } else {
+        res.status(404).json({ error: "Nothing found" })
+    }
+})
+
+app.post('/tasks', (req, res) => {
+    const title = req.body.title
+
+    const newTask = { id: tasks.length + 1, title }
+
+    tasks.push(newTask)
+
+    res.json({ data: newTask })
+})
+
+app.put('/tasks/:id', (req, res) => {
+    const taskId = parseInt(req.params.id, 10)
+
+    const taskIndex = tasks.findIndex(task => task.id === taskId)
+
+    if (taskIndex !== -1) {
+        tasks[taskIndex].title = req.body.title
+        res.json(tasks[taskIndex])
+    } else {
+        res.status(404).json({ error: "Task not found" })
+    }
+})
+
+app.delete('/tasks/:id', (req, res) => {
+    const taskId = parseInt(req.params.id, 10)
+
+    const taskIndex = tasks.findIndex(task => task.id === taskId)
+
+    if (taskIndex !== -1) {
+        tasks.splice(taskIndex, 1)
+        res.status(204).json({data: {id: taskId}})
+    } else {
+        res.status(404).json({ error: "Task not found" })
+    }
+})
+
+//отрабатывает если не один роутер не сработал
+app.use((req, res) => {
+    res.status(404).json({error: 'Nothing found'})
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
